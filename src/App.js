543ce033e6dbe3fb8c11a1abd48cd5212eabc4ba
tsukker <tsukker@users.noreply.github.com>
2021-12-07 21:21:00 +0900

    Read "Showing the Posts List"

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
 } from 'react-router-dom';
 import { Navbar } from './app/Navbar';
+import { PostsList } from './features/posts/PostsList';
 
 function App() {
   return (
@@ -17,9 +18,9 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <React.Fragment>
+                <PostsList />
+              </React.Fragment>
             )}
           />
           <Redirect to="/" />
